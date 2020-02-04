b742f86e47d9a01b6001c941686c7a28898470f4
Alexandra Vallée <alexandra-vallee@hotmail.fr>
2020-02-04 17:29:48 +0100

    formatage profil

--- controleur/controleurUniImg.php
+++ controleur/controleurUniImg.php
@@ -19,16 +19,18 @@ class ControleurUniImg extends ControleurUser
 
     	if(isset($_GET['id'])){
     		$image_find=$this->file->getFile($_GET['id'], null, null, null);
+    		$erreur = "";
     	}
     	elseif (isset($_GET['lien'])){
     		$image_find=$this->file->getFile(null, null,$_GET['lien'], null);
+    		$erreur = "";
     	}
     	else{
     		$erreur = "une erreur est survenue";
     	}
         
         $vue = new Vue("Image", $this->login);
-        $vue->generer(array('image' => $image_find[0], 'erreur' => $erreur));
+        $vue->generer(array('image' => $image_find[0], 'erreur' => $erreur, ));
     }
 }
 
