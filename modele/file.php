96183fdc9ca7f65a70d1747002bbcaea01c38d39
amandineLecuyer <amandine.lecuyer35@gmail.com>
2020-02-04 12:03:26 +0100

    no message

--- modele/file.php
+++ modele/file.php
@@ -16,7 +16,7 @@ class File extends connexion_bdd
     	 try
         {
             $ajoutFile ="INSERT INTO fichier(nom, lien_local, statut, id_user, lien_url, date_ajout ) VALUES(?, ?, ?, (SELECT ID FROM user WHERE pseudo=?),?,?) "; 
-            $param=[array(1,$nom,PDO::PARAM_STR),array(2,$lien,PDO::PARAM_STR),array(3,$this->statut,PDO::PARAM_STR),array(4,$this->user,PDO::PARAM_STR),array(5,$lienUrl,PDO::PARAM_STR),array(6,$dateAjout,PDO::PARAM_STR)];
+            $param=[array(1,$nom,PDO::PARAM_STR),array(2,$lien,PDO::PARAM_STR),array(3,$statut,PDO::PARAM_STR),array(4,$this->user,PDO::PARAM_STR),array(5,$lienUrl,PDO::PARAM_STR),array(6,$dateAjout,PDO::PARAM_STR)];
             $ajoutFile=$this->executerRequete($ajoutFile,$param);
         }
         catch(Exception $e)
