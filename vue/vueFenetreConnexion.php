e1d5c06e33feafebfac8c61ce133eb556e8ff769
chrystelle-hub <chrystelle.damide@gmail.com>
2020-02-05 17:27:03 +0100

    modification fenetre connexion

--- vue/vueFenetreConnexion.php
+++ vue/vueFenetreConnexion.php
@@ -1,34 +1,43 @@
 
 <div id="fenetreConnexion" aria-hidden="true" class="fenetreClose">
-   <button type="button" onclick="hideFenetreConnexion()">X</button> 
+   <button class="btn btn-secondary btn-sm" type="button" onclick="hideFenetreConnexion()">X</button> 
     <div>
     <div>
-    <h2>Connexion</h2>
-    <form id="formulaireConnexion" action="" method="POST"><div id='erreur'></div>
-        <label for="pseudo">Pseudo:</label>
-        <input type="text" id="pseudo_connexion" name="pseudo_connexion"/> <br>
-        <label for="mot_de_passe">Mot de passe:</label> 
-        <input type="password" id="mot_de_passe_connexion" name="mot_de_passe_connexion"/>  <br>    
-        <label for="resterConnecter">rester connecté</label>
-        <input type="checkbox" id="resterConnecter" name="resterConnecter"> <br>
-        <input type="submit" name='submitConnexion'value="login"/>
-        <br>
+    <h2 style="text-align:center">Connexion</h2>
+    <div class="container">
+        <form id="formulaireConnexion" action="" method="POST"><div id='erreur'></div>
+            <div class="form-group">
+                <label for="pseudo">Pseudo:</label>
+                <input class="form-control" type="text" id="pseudo_connexion" name="pseudo_connexion"/> <br>
+                <label for="mot_de_passe">Mot de passe:</label> 
+                <input class="form-control" type="password" id="mot_de_passe_connexion" name="mot_de_passe_connexion"/>  <br>    
+                <label for="resterConnecter">rester connecté</label>
+                <input type="checkbox" id="resterConnecter" name="resterConnecter"> <br>
+                <input class="btn btn-secondary" type="submit" name='submitConnexion'value="login"/>
+                
+                <br>
+            </div>
+    </div>
       
     </form> 
     </div>
     <div>
-    <h2>Pas encore inscrit?</h2>
-    <form id="formulaireInscription"action="" method="POST"><div id='erreur2'></div>
-        <label for="pseudo">Pseudo:</label> 
-        <input type="text" id="pseudo" name="pseudo"/><br>
-        <label for="email">Adresse électronique:</label> 
-        <input type="email" id="email" name="email"/><br>
-        <label for="mot_de_passe">Mot de passe:</label> 
-        <input type="password" id="mot_de_passe" name="mot_de_passe"/><br>
-        <label for="mot_de_passe2">Répéter le mot de passe:</label> 
-        <input type="password" id="mot_de_passe2" name="mot_de_passe2"/>  <br>      
-        <input type="submit" name='submitInscription'value="inscription"/>
-    </form> 
+    <h2 style="text-align:center">Pas encore inscrit?</h2>
+    <div class="container">
+        <form id="formulaireInscription"action="" method="POST"><div id='erreur2'></div>
+            <div class="form-group">
+                <label for="pseudo">Pseudo:</label> 
+                <input class="form-control" type="text" id="pseudo" name="pseudo"/><br>
+                <label for="email">Adresse électronique:</label> 
+                <input class="form-control" type="email" id="email" name="email"/><br>
+                <label for="mot_de_passe">Mot de passe:</label> 
+                <input class="form-control" type="password" id="mot_de_passe" name="mot_de_passe"/><br>
+                <label for="mot_de_passe2">Répéter le mot de passe:</label> 
+                <input class="form-control" type="password" id="mot_de_passe2" name="mot_de_passe2"/>  <br>      
+                <input class="btn btn-secondary mb-2" type="submit" name='submitInscription'value="inscription"/>
+            </div>
+        </form>
+    </div> 
     </div>
 </div>
 </div>
