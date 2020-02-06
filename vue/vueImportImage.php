3ae8aa3c8bbeb2b99b5ddfcc92daf0ec1d7f4012
Alexandra Vallée <alexandra-vallee@hotmail.fr>
2020-02-06 16:59:43 +0100

    pas d'apperçu si l'élément envoyer n'est pas une image

--- vue/vueImportImage.php
+++ vue/vueImportImage.php
@@ -46,10 +46,20 @@
 </script>
 
 	<script type="text/javascript">
+	function get_extension(filename) {
+    return filename.slice((filename.lastIndexOf('.') - 1 >>> 0) + 2);
+}
 	function charge(){
+
 		 $('#affiche').children().remove();
 		var file=event.target.files[0];
-		$(document).ready(function(){
+		var extention = '.'+get_extension(file['name']);
+		console.log(extention);
+		var extentions = ['.jpg', '.png', '.jpeg', '.gif'];
+		for(var i=0; i<extentions.length; i++) {
+			if(extention === extentions[i]) {
+			
+			$(document).ready(function(){
 			var reader = new FileReader();
             reader.onload = function (e)
 		{
@@ -58,6 +68,12 @@
 		}
 		reader.readAsDataURL(file);
 
-		})};
+		})
+
+			}
+		}
+
+		
+	};
 
 	</script>
